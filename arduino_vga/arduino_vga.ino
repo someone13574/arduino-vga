bf1a9ec2aeb02c98e4f92efdc07563e0f313760e
Owen Law <owenlaw222@gmail.com>
2022-11-16 21:23:58 -0500

    Rewrite in assembly

--- arduino_vga/arduino_vga.ino
+++ arduino_vga/arduino_vga.ino
@@ -1,103 +1,209 @@
-const float arduino_clock_time_us = 0.0625;
-const float pixel_clock_time_us = 0.1588877856;
+// X = r27:r26
+// Y = r29:r28
+// Z = r31:r30
 
-const unsigned int horizontal_visible_area_pixels = 160;
-const unsigned int horizontal_front_porch_pixels = 4;
-const unsigned int horizontal_sync_pulse_pixels = 24;
-const unsigned int horizontal_back_porch_pixels = 12;
-const unsigned int horizontal_total = horizontal_visible_area_pixels + horizontal_front_porch_pixels + horizontal_sync_pulse_pixels + horizontal_back_porch_pixels;
-
-const unsigned int vertical_visible_area_lines = 480;
-const unsigned int vertical_front_porch_lines = 10;
-const unsigned int vertical_sync_pulse_lines = 2;
-const unsigned int vertical_back_porch_lines = 33;
-
-const unsigned int line_index = 0;
-
-inline __attribute__((always_inline)) void Horizontal_Sync_And_Blank();
-inline __attribute__((always_inline)) void Constant_Horizontal_Line();
+inline void Visible_Line();
+__attribute__((always_inline));
+inline void Front_Porch_Line();
+__attribute__((always_inline));
+inline void Sync_Pulse_Line();
+__attribute__((always_inline));
+inline void Back_Porch_Line();
+__attribute__((always_inline));
 
 void setup()
 {
-  // Set digital pins 2-7 as outputs
-  DDRD &= 0b11111100;
+  // Set pins 2-12 to outputs
+  asm("ldi r18, 0b11111100");
+  asm("out 0x04, r18"); // Set DDRB
 
-  // Set digital pins 8-12 as outputs
-  DDRB &= 0b00011111;
+  asm("ldi r18, 0b00011111");
+  asm("out 0x2a, r18"); // Set DDRD
 }
 
 void loop()
 {
-  while (true)
-  {
-    line_index = 0;
+  asm("start_of_frame:");
 
-    // Vertical visible area
-    while (line_index < vertical_visible_area_lines)
-    {
-      PORTD |= 0b11111100;
-      PORTB |= 0b00000111;
-      Constant_Horizontal_Line();
+  // Visible area (244051.6385 cycle budget) 243843
+  asm("ldi ZL, lo8(480)"); // 1
+  asm("ldi ZH, hi8(480)"); // 1
 
-      line_index++;
-    }
+  asm("visible_area:");
+  Visible_Line();           // 504
+  asm("sbiw Z, 1");         // 2
+  asm("brne visible_area"); // 2/1
 
-    // Vertical Front porch
-    PORTD &= 0b00000011;
-    PORTB &= 0b11111000;
-    while (line_index < vertical_visible_area_lines + vertical_front_porch_lines)
-    {
-      Constant_Horizontal_Line();
+  // Front porch (5084.4091 cycle budget) 5052
+  asm("ldi r22, 10"); // 1
 
-      line_index++;
-    }
+  asm("front_porch:");
+  Front_Porch_Line();      // 505
+  asm("dec r22");          // 1
+  asm("brne front_porch"); // 2/1
 
-    // Vertical Sync Pulse
-    PORTB &= 0b11101111;
-    while (line_index < vertical_visible_area_lines + vertical_front_porch_lines + vertical_sync_pulse_lines)
-    {
-      Constant_Horizontal_Line();
+  // Sync pulse (1016.8818 cycle budget) 1014
+  asm("ldi r21, 0b00001000"); // 1
+  asm("out 0x05, r21");       // 1
+  asm("ldi r22, 2");          // 1
 
-      line_index++;
-    }
-    PORTB |= 0b00010000;
+  asm("sync_pulse:");
+  Sync_Pulse_Line();      // 505
+  asm("dec r22");         // 1
+  asm("brne sync_pulse"); // 2/1
 
-    // Vertical Back Porch
-    while (line_index < vertical_visible_area_lines + vertical_front_porch_lines + vertical_sync_pulse_lines + vertical_back_porch_lines)
-    {
-      Constant_Horizontal_Line();
+  asm("ldi r21, 0b00011000"); // 1
+  asm("out 0x05, r21");       // 1
 
-      line_index++;
-    }
-  }
+  // Back porch (16778.5501 cycle budget) // 16669
+  asm("ldi r22, 33"); // 1
+
+  asm("back_porch:");
+  Back_Porch_Line();      // 505
+  asm("dec r22");         // 1
+  asm("brne back_porch"); // 2/1
+
+  asm("jmp start_of_frame"); // 2
 }
 
-void Horizontal_Sync_And_Blank()
+void Visible_Line() // 504
 {
-  // Front porch
-  PORTD &= 0b00000011;
-  PORTB &= 0b11111000;
-  __builtin_avr_delay_cycles(horizontal_front_porch_pixels * pixel_clock_time_us / arduino_clock_time_us);
+  // Visible area (406.7527 cycle budget, no restrictions) 407
+  asm("ldi r25, 133"); // 1
 
-  // Sync pulse
-  PORTB &= 0b11110111;
-  __builtin_avr_delay_cycles(horizontal_sync_pulse_pixels * pixel_clock_time_us / arduino_clock_time_us);
-  PORTB |= 0b00001000;
+  asm("visible_line_visible_area:");
+  asm("dec r25");                        // 1
+  asm("brne visible_line_visible_area"); // 2/1
 
-  // Back porch
-  __builtin_avr_delay_cycles(horizontal_back_porch_pixels * pixel_clock_time_us / arduino_clock_time_us);
+  asm("ldi r25, 0b00000000"); // 1
+  asm("ldi r24, 0b00011000"); // 1
+  asm("out 0x05, r24");       // 1     PORTB
+  asm("out 0x0b, r25");       // 1     PORTD
+  asm("ldi r25, 2");          // 1 (set for front porch counter)
+  asm("nop \n nop");          // 1
+
+  // Front porch (10.1688 cycle budget, enter restriction) 10
+  asm("visible_line_front_porch:");
+  asm("dec r25");                       // 1
+  asm("brne visible_line_front_porch"); // 2/1
+
+  asm("ldi r25, 19");         // 1 (set for sync pulse counter)
+  asm("ldi r24, 0b00010000"); // 1 (sync pulse set state)
+  asm("ldi r23, 0b00011000"); // 1 (sync pulse resolve state)
+
+  // Sync pulse (61.0129 cycle budget, enter restriction) 61
+  asm("out 0x05, r24"); // 1
+
+  asm("visible_line_sync_pulse:");
+  asm("dec r25");                      // 1
+  asm("brne visible_line_sync_pulse"); // 2/1
+
+  asm("nop");           // 1
+  asm("out 0x05, r24"); // 1
+
+  // Back porch (30.5065 cycle budget, enter restriction) 26
+  asm("ldi r25, 8"); // 1
+
+  asm("visible_line_back_porch:");
+  asm("dec r25");                      // 1
+  asm("brne visible_line_back_porch"); // 2/1
 }
 
-void Constant_Horizontal_Line()
+void Front_Porch_Line() // 505
 {
-  // Visible area and front porch
-  __builtin_avr_delay_cycles((horizontal_visible_area_pixels + horizontal_front_porch_pixels) * pixel_clock_time_us / arduino_clock_time_us);
+  // Front porch (416.9215 cycle budget) 417
+  asm("ldi r25, 137"); // 1
 
-  // Sync pulse
-  PORTB &= 0b11110111;
-  __builtin_avr_delay_cycles(horizontal_sync_pulse_pixels * pixel_clock_time_us / arduino_clock_time_us);
-  PORTB |= 0b00001000;
+  asm("front_porch_line_front_porch:");
+  asm("dec r25");                           // 1
+  asm("brne front_porch_line_front_porch"); // 2/1
 
-  // Back porch
-  __builtin_avr_delay_cycles(horizontal_back_porch_pixels * pixel_clock_time_us / arduino_clock_time_us);
+  asm("ldi r25, 19");         // 1 (set for sync pulse counter)
+  asm("ldi r24, 0b00010000"); // 1 (sync pulse set state)
+  asm("ldi r23, 0b00011000"); // 1 (sync pulse resolve state)
+  asm("nop");                 // 1
+
+  // Sync pulse (61.0129 cycle budget, enter restriction) 61
+  asm("out 0x05, r24"); // 1
+
+  asm("front_porch_line_sync_pulse:");
+  asm("dec r25");                          // 1
+  asm("brne front_porch_line_sync_pulse"); // 2/1
+
+  asm("nop");           // 1
+  asm("out 0x05, r24"); // 1
+
+  // Back porch (30.5065 cycle budget, enter restriction) 27
+  asm("ldi r25, 8"); // 1
+
+  asm("front_porch_line_back_porch:");
+  asm("dec r25");                          // 1
+  asm("brne front_porch_line_back_porch"); // 2/1
+  asm("nop");                              // 1
+}
+
+void Sync_Pulse_Line() // 505
+{
+  // Front porch (416.9215 cycle budget) 417
+  asm("ldi r25, 137"); // 1
+
+  asm("sync_pulse_line_front_porch:");
+  asm("dec r25");                          // 1
+  asm("brne sync_pulse_line_front_porch"); // 2/1
+
+  asm("ldi r25, 19");         // 1 (set for sync pulse counter)
+  asm("ldi r24, 0b00000000"); // 1 (sync pulse set state)
+  asm("ldi r23, 0b00001000"); // 1 (sync pulse resolve state)
+  asm("nop");                 // 1
+
+  // Sync pulse (61.0129 cycle budget, enter restriction) 61
+  asm("out 0x05, r24"); // 1
+
+  asm("sync_pulse_line_sync_pulse:");
+  asm("dec r25");                         // 1
+  asm("brne sync_pulse_line_sync_pulse"); // 2/1
+
+  asm("nop");           // 1
+  asm("out 0x05, r24"); // 1
+
+  // Back porch (30.5065 cycle budget, enter restriction) 27
+  asm("ldi r25, 8"); // 1
+
+  asm("sync_pulse_line_back_porch:");
+  asm("dec r25");                         // 1
+  asm("brne sync_pulse_line_back_porch"); // 2/1
+  asm("nop");                             // 1
+}
+
+void Back_Porch_Line() // 505
+{
+  // Front porch (416.9215 cycle budget) 417
+  asm("ldi r25, 137"); // 1
+
+  asm("back_porch_line_front_porch:");
+  asm("dec r25");                          // 1
+  asm("brne back_porch_line_front_porch"); // 2/1
+
+  asm("ldi r25, 19");         // 1 (set for sync pulse counter)
+  asm("ldi r24, 0b00010000"); // 1 (sync pulse set state)
+  asm("ldi r23, 0b00011000"); // 1 (sync pulse resolve state)
+  asm("nop");                 // 1
+
+  // Sync pulse (61.0129 cycle budget, enter restriction) 61
+  asm("out 0x05, r24"); // 1
+
+  asm("back_porch_line_sync_pulse:");
+  asm("dec r25");                         // 1
+  asm("brne back_porch_line_sync_pulse"); // 2/1
+
+  asm("nop");           // 1
+  asm("out 0x05, r24"); // 1
+
+  // Back porch (30.5065 cycle budget, enter restriction) 27
+  asm("ldi r25, 8"); // 1
+
+  asm("back_porch_line_back_porch:");
+  asm("dec r25");                         // 1
+  asm("brne back_porch_line_back_porch"); // 2/1
+  asm("nop");                             // 1
 }
